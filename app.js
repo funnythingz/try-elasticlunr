ce80534e2a57c2a6872955a606dd9515dae7f90e
funnythingz <funnythingz69+github@gmail.com>
2019-04-18 11:12:25 +0900

    can use japanese search

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const elasticlunr = require('elasticlunr');
+require('./lib/lunr.stemmer.support.js')(elasticlunr);
+require('./lib/lunr.jp.js')(elasticlunr);
 
 const index = elasticlunr(function() {
+  this.use(elasticlunr.jp);
   this.addField('body');
   this.setRef('id');
 });
@@ -15,6 +18,16 @@ index.addDoc({
   body: 'hello! how? low.'
 });
 
+index.addDoc({
+  id: 3,
+  body: 'うんこ ごはん カレー。'
+});
+
+index.addDoc({
+  id: 4,
+  body: 'こんにちは！元気？どんな感じ？'
+});
+
 //単語一致
 console.log(index.search('curry'));
 
@@ -23,5 +36,16 @@ console.log(index.search('curr'));
 
 //部分検索一致
 console.log(index.search('h', {
+  expand: true
+}));
+
+//単語一致
+console.log(index.search('カレー'));
+
+//部分検索一致しない
+console.log(index.search('こ'));
+
+//部分検索一致
+console.log(index.search('元気', {
   expand: true
 }));
